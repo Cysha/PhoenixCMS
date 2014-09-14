002a07ddfaae15a18be24f20be6f0efa9e4af9b6
xLink <xLink@cybershade.org>
2014-09-14 21:32:43 +0100

    theme update

--- public/themes/default/layouts/cols-2-left.blade.php
+++ public/themes/default/layouts/cols-2-left.blade.php
@@ -19,7 +19,6 @@
 {{ Assets::js() }}
 {{ Theme::asset()->scripts() }}
 
-
 <!--[if lt IE 9]>
     <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
---
+++ public/themes/default/views/partials/theme/errors/404.blade.php
@@ -0,0 +1,3 @@
+<div class="page-header">
+    <h1>404 Page Not Found...</h1>
+</div>
