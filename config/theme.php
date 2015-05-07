92b6279861509c236ae02680e4917c5812de84f8
Dan Aldridge <xLink@cybershade.org>
2015-05-07 17:01:56 +0100

    fixed some paths up for the themes

--- config/theme.php
+++ config/theme.php
@@ -36,7 +36,7 @@ return array(
     |
     */
 
-    'layoutDefault' => 'default',
+    'layoutDefault' => 'basic',
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return array(
         'asset'   => 'assets',
         'partial' => 'views/partials',
         'widget'  => 'widgets',
-        'view'    => 'partials'
+        'view'    => 'views'
     ),
 
     /*
