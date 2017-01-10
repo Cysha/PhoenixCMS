050c65ea4aa6000ad4cd2a11c79fa0a89a4b9c13
Dan Aldridge <xLink@cybershade.org>
2017-01-10 12:48:56 +0000

    added travis to repo
    switched to in mem sqlitedb
    
    killed dummy env file
    
    added the testing sqlite db
    
    switched up composer and cms installs in travis
    
    re-added .env.testing updated .travis.yml to compensate
    
    updating deps
    
    switched --force to --no-interaction
    
    update deps..again
    
    removed php 5.5 from the travis config
    
    deps actually updated
    
    tests are working...now for the modules
    
    remove hhvm from travis
    
    upped some small travis changes

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,19 +1,15 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic functional test example.
-     *
-     * @return void
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $this->visit('/');
+
+        $this->assertResponseOk();
     }
 }
